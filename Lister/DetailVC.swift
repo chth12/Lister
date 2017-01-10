53b14874317e4d1b724c971a3523f22681362bfd
chth12 <ctoddh12@gmail.com>
2017-01-10 14:39:48 -0500

    UI

--- Lister/DetailVC.swift
+++ Lister/DetailVC.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DetailVC: UIViewController {
 
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
