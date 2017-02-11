bffa1c705d16c4a9bd4db86918067704850e0fa3
chth12 <ctoddh12@gmail.com>
2017-02-11 12:20:33 -0500

    Auth

--- Lister/RegisterUserVC.swift
+++ Lister/RegisterUserVC.swift
@@ -15,18 +15,21 @@ class RegisterUserVC: UIViewController {
     
     @IBOutlet weak var emailAddress: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var reEnterPassword: UITextField!
 
-   
-    
-   
-    @IBAction func registerButtonPressed(_ sender: UIButton) {
-        FIRAuth.auth()?.createUser(withEmail: "e", password: "pass", completion: {
-            (user, error) in
-            if error != nil {
-                print("\(error)")
-            }
+    @IBAction func registerButtonPressed(_ sender: Any?) {
+        print("TODD: button pressed")
+        if emailAddress != nil || password != nil || reEnterPassword != nil{
+            FIRAuth.auth()?.createUser(withEmail: emailAddress.text! as String, password: password.text! as String, completion: {
+                (user, error) in
+                if error != nil {
+                    print("TODD2: \(error!)")
+                    self.emailAddress.borderStyle = UITextBorderStyle.roundedRect
+                }
+            })
+            } else {
             
-        })
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
